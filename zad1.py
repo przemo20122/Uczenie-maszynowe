0857191ca83d40a66b974a10a47d3ba6105d088b
Przemek <77122883+przemo20122@users.noreply.github.com>
2022-10-17 15:42:11 +0200

    update
    zad1, zad3

--- zad1.py
+++ zad1.py
@@ -5,34 +5,36 @@ Created on Mon Oct 17 13:33:18 2022
 @author: Przemek
 """
 
+
 # lab 3
 class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
-    
+
     @property
     def name(self):
         return self.name
+
     def marks(self):
         return self.marks
-    
+
     @name.setter
     def name(self, name: str):
-        self._name = name    
-        
+        self._name = name
+
     def marks(self, value: int):
         self._marks = value
-    
+
     def is_passed(self) -> bool:
-        if(self.marks > 50):
+        if (self.marks > 50):
             return True
         else:
             return False
+
 
 student1 = Student("Przemek", 56)
 student2 = Student("Monika", 42)
 
 print(student1.is_passed())
 print(student2.is_passed())
-        
--- zad3.py
+++ zad3.py
@@ -5,32 +5,35 @@ Created on Mon Oct 17 14:19:58 2022
 @author: Przemek
 """
 
+
 class Property():
     def __init__(self, area, rooms: int, price, address):
         self.area = area
         self.rooms = rooms
         self.price = price
         self.address = address
-        
+
+
 class House(Property):
-    def __init__(self,area, rooms: int, price, address, plot: int):
-        super().__init__(area ,rooms, price, address)
+    def __init__(self, area, rooms: int, price, address, plot: int):
+        super().__init__(area, rooms, price, address)
         self.plot = plot
-    
+
     def __str__(self):
         return "{} {} {} {} {}".format(self.area, self.rooms, self.price, self.address, self.plot)
-        
+
+
 class Flat(Property):
     def __init__(self, area, rooms: int, price, address, floor):
         super().__init__(area, rooms, price, address)
         self.floor = floor
-    
+
     def __str__(self):
         return "{} {} {} {} {}".format(self.area, self.rooms, self.price, self.address, self.floor)
-        
-    
-domek1 = House("Katowice",5,800000,"Bogucice",34)
-mieszkanie1 = Flat("Katowice",3,500000,"Bogucice",120)
+
+
+domek1 = House("Katowice", 5, 800000, "Bogucice", 34)
+mieszkanie1 = Flat("Katowice", 3, 500000, "Bogucice", 120)
 
 print(domek1)
 print(mieszkanie1)

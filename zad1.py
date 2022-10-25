f5a8073732b3c0071b73676c8731e194a15c4ad1
Przemek <77122883+przemo20122@users.noreply.github.com>
2022-10-25 14:53:18 +0200

    Add function image_to_string
    dodanie funkcji image_to_string

--- zad1.py
+++ zad1.py
@@ -8,15 +8,25 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 '''
 import cv2
-from PIL import Image
 import pytesseract
 
+# ścieżka do pytesseract.exe
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-print(pytesseract.image_to_string(Image.open('three.png')))
-img_cv = cv2.imread('three.png')
 
-# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
-# we need to convert from BGR to RGB format/mode:
-img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-print(pytesseract.image_to_string(img_rgb))
-print(pytesseract.image_to_string(img_rgb))
+
+def image_to_string(img_path):
+    img = cv2.imread(img_path)
+
+    # Preprocessing (zmiana BGR-czarnobiały, progowanie obrazu, Rozmycie gaussowskie)
+    img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_g = cv2.threshold(img_g, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    img_g = cv2.GaussianBlur(img_g, (5, 5), 0)
+
+    ocr = pytesseract.image_to_string(img_g)
+    print(ocr)
+
+
+image_to_string('tekst1.png')
+image_to_string('tekst2.png')
+image_to_string('tekst3.png')
+image_to_string('tekst4.png')

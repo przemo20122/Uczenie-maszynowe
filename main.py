f9ce3dd9af48cc3a79d960ca2758a7c9adc94893
Przemek <77122883+przemo20122@users.noreply.github.com>
2022-12-11 14:44:26 +0100

    Update main.py
    połączenie color extraction z background substraction

--- main.py
+++ main.py
@@ -4,27 +4,32 @@ import numpy as np
 cap = cv2.VideoCapture('one round kam4.mp4')
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 out = cv2.VideoWriter('output_video.mp4', fourcc, 30, (1920, 1080))
+background_substraction = cv2.createBackgroundSubtractorMOG2()
 
 while 1:
     st, frame = cap.read()
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower = np.array([30, 150, 50])
-    upper = np.array([250, 255, 180])
-    mask = cv2.inRange(img, lower, upper)
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    #cv2.imshow('frame', fgmask)
-    out.write(result)
 
+    # lower mask red
+    lower_red = np.array([0, 170, 50])
+    upper_red = np.array([5, 255, 255])
+    mask_red1 = cv2.inRange(img, lower_red, upper_red)
+
+    # upper mask red
+    lower_red = np.array([170, 150, 50])
+    upper_red = np.array([180, 255, 255])
+    mask_red2 = cv2.inRange(img, lower_red, upper_red)
+
+    # mask blue
+    lower_blue = np.array([100, 80, 2])
+    upper_blue = np.array([129, 255, 255])
+    mask_blue = cv2.inRange(img, lower_blue, upper_blue)
+
+    result = cv2.bitwise_and(frame, frame, mask=mask_red1 + mask_red2 + mask_blue)
+    background_mask = background_substraction.apply(frame)
+    out.write(cv2.copyTo(result, background_mask))
+    # out.write(result)
 
 cap.release()
 cv2.waitKey()
 cv2.destroyAllWindows()
-
-# img = cv2.imread('frame1.png')
-# result = img.copy()
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-# lower_blue = np.array([100,50,50])
-# upper_blue = np.array([130,255,255])
-# mask = cv2.inRange(img, lower_blue, upper_blue)
-# result = cv2.bitwise_and(result, result, mask=mask)
-# cv2.imshow('result', result)

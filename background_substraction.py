1a0243d99fac0ff683aa21a32efcb0576985e3ea
Przemek <77122883+przemo20122@users.noreply.github.com>
2022-12-11 14:46:23 +0100

    Update background_substraction.py
    update background_substraction

--- background_substraction.py
+++ background_substraction.py
@@ -4,22 +4,16 @@ import numpy as np
 cap = cv2.VideoCapture('output_video.mp4')
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 out = cv2.VideoWriter('output_video2.mp4', fourcc, 30, (1920, 1080))
-fgbg = cv2.createBackgroundSubtractorMOG2()
-fgbg = cv2.createBackgroundSubtractorKNN()
-
+background_substraction = cv2.createBackgroundSubtractorMOG2()
+# background_substraction = cv2.createBackgroundSubtractorKNN()
 
 while 1:
     st, frame = cap.read()
-    fgmask = fgbg.apply(frame)
-    img = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
-    cv2.imshow('FG Mask', fgmask)
-    cv2.imshow('img', img)
-    out.write(img)
-    if cv2.waitKey(30) & 0xFF == ord('q'):
-        break
-    #out.write(fgmask)
-
+    background_mask = background_substraction.apply(frame)
+    result = cv2.cvtColor(background_mask, cv2.COLOR_GRAY2BGR)
+    out.write(result)
+    # if cv2.waitKey(30) & 0xFF == ord('q'):
+    #    break
 
 cap.release()
-cv2.waitKey()
-cv2.destroyAllWindows()
+out.release()
